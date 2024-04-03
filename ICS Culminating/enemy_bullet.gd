2d2d7a5d38909ff021108e5fd8f7b47322086daa
sharkyx24 <161484372+sharkyx24@users.noreply.github.com>
2024-04-03 10:05:46 -0400

    Add files via upload

--- ICS Culminating/enemy_bullet.gd
+++ ICS Culminating/enemy_bullet.gd
@@ -1,18 +1,17 @@
 extends Area2D
-var speed = 750
-
+@export var speed = 5
+@export var bullet : PackedScene
+var direction := Vector2.ZERO
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	position += transform.x * speed * delta
-	pass
-
-
-func _on_body_entered(body):
-	if body.is_in_group("mobs"):
-		body.queue_free()
-	pass # Replace with function body.
+func _physics_process(delta):
+	if direction != Vector2.ZERO:
+		var velocity = direction * speed
+		global_position += velocity
+		
+func set_direction(direction: Vector2):
+	self.direction = direction
