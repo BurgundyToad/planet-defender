f39c55bde6513d89e86f83b4eb06d952f563f8c9
sharkyx24 <161484372+sharkyx24@users.noreply.github.com>
2024-04-17 09:15:03 -0400

    Add files via upload

--- ICS Culminating/Level1 Scene.gd
+++ ICS Culminating/Level1 Scene.gd
@@ -1,13 +1,33 @@
 extends Node2D
-
-
+@export var mob_scene: PackedScene
+var amount_mobs = 12
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$MobTimer.start()
 	
+	
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
 func _process(delta):
 	pass
+
+
+func _on_mob_timer_timeout():
+	if amount_mobs > 0: 
+		var mob = mob_scene.instantiate()
+		var mob_spawn_location = $MobPath/MobSpawnLocation
+		mob_spawn_location.progress_ratio = randf()
+		var direction = mob_spawn_location.rotation + PI/2
+		
+		mob.position = mob_spawn_location.position
+		direction += randf_range(-PI/4, PI/4)
+		mob.rotation = direction
+		
+		add_child(mob)
+		amount_mobs -= 1
+		print(amount_mobs)
+	pass # Replace with function body.
