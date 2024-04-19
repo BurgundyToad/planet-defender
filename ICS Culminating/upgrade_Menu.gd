29aef3819f1434594908020d7cf462c2ec01b7ae
BurgundyToad <146752568+BurgundyToad@users.noreply.github.com>
2024-04-19 08:58:39 -0400

    towers wip II (i forgot these ones)

--- ICS Culminating/upgrade_Menu.gd
+++ ICS Culminating/upgrade_Menu.gd
@@ -1,9 +1,11 @@
 extends Control
-
+class_name UpgradeMenu
 @export var Upgrade_Menu : Tower
+signal shooter_1_pressed
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	hide()
+	emit_signal("shooter_1_pressed", shooter_1_pressed)
 	Upgrade_Menu.connect("interacted", _on_interated)
 	Upgrade_Menu.connect("moved_away", _on_moved_away)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,3 +18,8 @@ func _on_interated(interated):
 func _on_moved_away(moved_away):
 	if(moved_away):
 		hide()
+
+
+func _on_button_pressed():
+	emit_signal("shooter_1_pressed", shooter_1_pressed)
+	pass # Replace with function body.
