4b73ce4a38696855c6c6e507f03dbc224542d923
BurgundyToad <146752568+BurgundyToad@users.noreply.github.com>
2024-04-23 14:30:18 -0400

    i am godot god (tower 1 implemented  and player shoots 360 again)

--- planet-defender-main (3) - Copy/planet-defender-main/ICS Culminating/basic_tower.gd
+++ planet-defender-main (3) - Copy/planet-defender-main/ICS Culminating/basic_tower.gd
@@ -1,11 +1,11 @@
 extends Area2D
-
 class_name Tower
 signal interacted
 var interaction_range = false
 @export var Basic_Tower : UpgradeMenu
+@export var shooter_turret : PackedScene
 signal moved_away
-var horrific_solution = 0
+var tower_exists = false
 func _ready():
 	$Interact_Sign.hide()
 	$Label.hide()
@@ -14,22 +14,19 @@ func _process(delta):
 		if interaction_range:
 			emit_signal("interacted", interacted)
 func _on_interaction_range_body_entered(body):
-	print('range entered')
 	if body.is_in_group("player"): 
-		print('entered code should run')
 		$Interact_Sign.show()
 		$Label.show()
 		interaction_range = true
 func _on_interaction_range_body_exited(body):
-	print ('range exited')
 	if body.is_in_group("player"): 
-		print("exit code should run")
 		$Interact_Sign.hide()
 		$Label.hide()
 		interaction_range = false
 		emit_signal("moved_away", moved_away)
-func on_shooter_1_pressed(shooter_1_pressed):
-	if horrific_solution == 0:
-		horrific_solution+=1
-	else:
-		print("test wotked")
+func on_shooter_1_pressed():
+	if not tower_exists:
+		tower_exists = true
+		var st = shooter_turret.instantiate()
+		owner.add_child(st)
+		st.transform = $AnimatedSprite2D/turret_location.global_transform
--- planet-defender-main (3) - Copy/planet-defender-main/ICS Culminating/shooter_turret.gd
+++ planet-defender-main (3) - Copy/planet-defender-main/ICS Culminating/shooter_turret.gd
@@ -1,9 +1,35 @@
 extends Area2D
 # Called when the node enters the scene tree for the first time.
+@export var Bullet : PackedScene
+var target
+var found_mob = false
+var cc
+var mob_pos = Vector2.ZERO
 func _ready():
+	print(is_inside_tree())
+	$shoot_timer.start()
 	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+func aim():
+	cc = get_parent().get_child_count()
+	found_mob = false
+	mob_pos = Vector2.ZERO
+	for i in range(0,cc):
+		target = get_parent().get_child(i)
+		if target.is_in_group("mobs") and found_mob == false:
+			found_mob = true
+			mob_pos = self.position-target.position
+	look_at(mob_pos)
+func shoot():
+	print('yeah')
+	var b = Bullet.instantiate()
+	get_parent().add_child(b)
+	b.transform = $sprite/turret_muzzle.global_transform
+	b.rotation = atan2(-mob_pos.y,-mob_pos.x)
+	b.scale = Vector2(1.1,1.1)
+func _on_shoot_timer_timeout():
+	aim()
+	if found_mob == true:
+		shoot()
