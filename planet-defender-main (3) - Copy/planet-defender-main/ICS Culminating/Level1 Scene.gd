158e9bfcd9ee35d2082eafe179774c31b8f5cf78
sharkyx24 <161484372+sharkyx24@users.noreply.github.com>
2024-04-25 10:31:57 -0400

    Add files via upload

--- planet-defender-main (3) - Copy/planet-defender-main/ICS Culminating/Level1 Scene.gd
+++ planet-defender-main (3) - Copy/planet-defender-main/ICS Culminating/Level1 Scene.gd
@@ -1,10 +1,14 @@
 extends Node2D
 @export var mob_scene: PackedScene
 @export var tower : Tower
-var amount_mobs = 12
+var amount_mobs = 3
+var wave = 5
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print("level 1 loaded")
 	$MobTimer.start()
+	$WaveTimer.start()
 	
 	
 	pass # Replace with function body.
@@ -15,8 +19,11 @@ func _ready():
 func _input(event: InputEvent):
 	if event.is_action_pressed('upgrade'):
 		$CanvasLayer2.visible = !$CanvasLayer2.visible
+
+
 func _on_mob_timer_timeout():
 	if amount_mobs > 0: 
+		$wave.hide()
 		var mob = mob_scene.instantiate()
 		var mob_spawn_location = $MobPath/MobSpawnLocation
 		mob_spawn_location.progress_ratio = randf()
@@ -28,5 +35,15 @@ func _on_mob_timer_timeout():
 		
 		add_child(mob)
 		amount_mobs -= 1
-		print(amount_mobs)
+		
+	pass # Replace with function body.
+
+
+func _on_wave_timer_timeout():
+	$wave.show()
+	await get_tree().create_timer(2.0).timeout
+	
+	if wave > 0:
+		amount_mobs = 3
+	wave -= 1
 	pass # Replace with function body.
