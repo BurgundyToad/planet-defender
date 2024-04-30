78be44a0d20862553922224f5c04798b4f25f998
sharkyx24 <161484372+sharkyx24@users.noreply.github.com>
2024-04-30 14:15:29 -0400

    Add files via upload

--- Level1 Scene.gd
+++ Level1 Scene.gd
@@ -2,22 +2,34 @@ extends Node2D
 @export var mob_scene: PackedScene
 @export var tower : Tower
 @export var Level_1 :PackedScene
-var amount_mobs = 12
+var amount_mobs = 3
+var wave = 4
+var spawn = 1
+signal wave_over
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	process_mode = Node.PROCESS_MODE_PAUSABLE
 	$MobTimer.start()
+	$WaveTimer.start()
+	$wave.show()
 	#if $Mouse_Moved_Player/CanvasLayer3 != null:
 		#$Mouse_Moved_Player/CanvasLayer3.show()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-
+func _process(delta):
+		if wave == 0:
+			await get_tree().create_timer(35.0).timeout
+			queue_free()
+			get_parent().get_node("Menus node").get_node("BackGround").show()
+			
 func _input(event: InputEvent):
 	if event.is_action_pressed('upgrade'):
 		$CanvasLayer2.visible = !$CanvasLayer2.visible
 func _on_mob_timer_timeout():
 	if amount_mobs > 0: 
+		$wave.hide()
 		var mob = mob_scene.instantiate()
 		var mob_spawn_location = $MobPath/MobSpawnLocation
 		mob_spawn_location.progress_ratio = randf()
@@ -28,6 +40,8 @@ func _on_mob_timer_timeout():
 		mob.rotation = direction
 		
 		add_child(mob)
+
+		
 		amount_mobs -= 1
 		print(amount_mobs)
 	pass # Replace with function body.
@@ -47,3 +61,26 @@ func _on_game_over_restart(_restart):
 func _on_game_over_quit(_quit):
 	queue_free()
 	get_parent().get_node("Menus node").get_node("BackGround").show()
+
+
+func _on_wave_timer_timeout():
+	
+	var wave_num = 6 - wave
+	$wave.set_text("Wave " + str(wave_num))
+	$wave.show()
+	await get_tree().create_timer(2.0).timeout
+	
+	if wave > 0:
+		spawn += 1
+		amount_mobs = 3 * spawn
+		print(amount_mobs)
+		wave -= 1
+	print(wave)
+
+	pass # Replace with function body.
+
+
+func _on_wave_over():
+	queue_free()
+	get_parent().get_node("Menus node").get_node("BackGround").show()
+	pass # Replace with function body.
