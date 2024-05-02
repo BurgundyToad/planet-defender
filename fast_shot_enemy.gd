845d66f067afe6c735a4eb4d2f757a2121b5e6b1
Spooksam6 <149258710+Spooksam6@users.noreply.github.com>
2024-05-02 14:16:44 -0400

    Add files via upload

--- fast_shot_enemy.gd
+++ fast_shot_enemy.gd
@@ -1,19 +1,18 @@
 extends RigidBody2D
-@export var speed = 500
+@export var Bullet : PackedScene
+@export var speed = 250
 var player_position
 var target_position
 var timer = 0
 @onready var player = get_parent().get_node("Mouse_Moved_Player")
-
-
+signal mob_fire_bullet(bullet) 
+var marked_for_death=false
 func _ready():
 	$AnimatedSprite2D.play()
-	#$ShootTimer.start()
-
+	$ShootTimer.start()
 func _physics_process(_delta):
 	player_position = player.position
 	target_position = (player_position - position).normalized()
-	
 	if position.distance_to(player_position) > 5:
 
 		linear_velocity = target_position * speed
@@ -25,11 +24,17 @@ func _physics_process(_delta):
 		look_at(player_position)
 		gravity_scale = 0
 
+func shoot(): 
+	var b = Bullet.instantiate()
+	get_parent().add_child(b)
+	b.transform = $AnimatedSprite2D/Muzzle.global_transform
+	var direction = (player_position - position).normalized()
+	var direction_to_mouse = direction
+	b.set_direction(direction_to_mouse)
+	emit_signal("mob_fire_bullet", b)
 
 
 
-func _on_area_2d_body_entered(body):
-	if body.is_in_group('player'):
-		for x in range(0,5):
-			body.damage()
-		queue_free()
+func _on_shoot_timer_timeout():
+	shoot()
+	pass # Replace with function body.
